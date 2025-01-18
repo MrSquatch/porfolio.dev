29c5858fb673028ace538724548f0c1b4d2b179c
MrSquatch <86651695+MrSquatch@users.noreply.github.com>
2025-01-18 00:40:04 -0500

    modificaciones generales para adaptar el porfolio a mi caso particular

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
-import { defineConfig } from 'astro/config'
-import tailwind from "@astrojs/tailwind"
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import robotsTxt from 'astro-robots-txt';
 
-import robotsTxt from "astro-robots-txt"
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), robotsTxt()],
-  site: 'https://porfolio.dev/'
-})
+  integrations: [tailwind(), robotsTxt(), icon({ iconDir: 'src/icons' })],
+  site: 'https://porfolio.dev/',
+});
